4db3f8299d17dc9f6b4958c5237d194409405bdc
drew gordon <drew.s.gordon@gmail.com>
2014-12-29 23:39:40 -0500

    add an argument for if we want to specify a boro or get all

--- topical_yak.py
+++ topical_yak.py
@@ -1,16 +1,36 @@
 from config import Data
 import connection as conn
+import sys
+
+try:
+	_BORO = sys.argv[1] 
+except:
+	_BORO = 'ALL'
 
 def getYaks(*args):
 
-	yaks = []
+	def whichYaks(a, b):
+		if len(b) == 0:
+			return a
+		else:
+			return b
+
+	boro = args[0]
+
+	allYaks = [] 
+	boroYaks = []
 
 	res = conn.collection.find()
 
-	for r in res:
-		yaks.append(r['text'])
 
-	yaks_corpus = [yak.lower().split() for yak in yaks]
+	for r in res:
+		if boro != 'ALL' and r['borough'].lower() == boro.lower():
+			boroYaks.append(r['text'])
+		else:
+			allYaks.append(r['text'])
+
+
+	yaks_corpus = [yak.lower().split() for yak in whichYaks(allYaks, boroYaks)]
 
 	return yaks_corpus
 
@@ -19,5 +39,5 @@ def getYaks(*args):
 
 
 if __name__ == '__main__':
-	allYaks = getYaks()
-	print allYaks
+	allYaks = getYaks(_BORO)
+	print len(allYaks)
