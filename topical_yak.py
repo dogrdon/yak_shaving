b9de9e04e040c088497e14cfcda2db00cd6f09a8
drew gordon <drew.s.gordon@gmail.com>
2014-12-30 12:50:24 -0500

    try MITIE for entity extraction
    this thing needs some training:
    PERSON: 😍😍😍😍😍😍😍i
    ORGANIZATION: mdma
    ORGANIZATION: oven &
    ORGANIZATION: sparta
    PERSON: tryna
    PERSON: schnabel
    LOCATION: buffalo
    PERSON: bae
    PERSON: nowi
    LOCATION: airport 😴
    LOCATION: cali
    LOCATION: vegas
    LOCATION: punta cana
    LOCATION: queens
    ORGANIZATION: buffalo
    PERSON: 😱😖😪what
    LOCATION: 4am
    LOCATION: ridgewood
    LOCATION: dac

--- topical_yak.py
+++ topical_yak.py
@@ -1,11 +1,17 @@
 from config import Data
 import connection as conn
-import sys
+import sys, os
+
+dirpath = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dirpath + '/../MITIE/mitielib')
+
+from mitie import *
 
 try:
 	_BORO = sys.argv[1] 
 except:
 	_BORO = 'ALL'
+	print "You can specify a Borough: Brooklyn, Manhattan, Queens, Bronx, Staten Island"
 
 def getYaks(*args):
 
@@ -14,6 +20,7 @@ def getYaks(*args):
 			return a
 		else:
 			return b
+
 
 	boro = args[0]
 
@@ -30,14 +37,44 @@ def getYaks(*args):
 			allYaks.append(r['text'])
 
 
-	yaks_corpus = [yak.lower().split() for yak in whichYaks(allYaks, boroYaks)]
+	yaks_in_lists = [yak.lower().split() for yak in whichYaks(allYaks, boroYaks)]
 
-	return yaks_corpus
+	return yaks_in_lists
+
+
+def generateTopics(text):
+	ner = named_entity_extractor('../MITIE/MITIE-models/english/ner_model.dat')
+	tokens = tokenize(text)
+	entities = ner.extract_entities(tokens)
+
+	print "\nEntities found: ", entities
+	print "\nThis many entitites: ", len(entities)
+
+	for e in entities:
+		range = e[0]
+		tag = e[1]
+		entity_text = " ".join(tokens[i] for i in range)
+		print "   " + tag + ": " + entity_text
+
+def makeCorpus(yaks):
+	txt = ""
+
+	for y in yaks:
+		txt += (" ").join(y).encode('utf-8').strip()
+
+	return txt
 
 
 
 
 
 if __name__ == '__main__':
-	allYaks = getYaks(_BORO)
-	print len(allYaks)
+	print "getting topics for", _BORO
+
+	yaksLoad = getYaks(_BORO)
+
+	yaks_dot_txt = makeCorpus(yaksLoad)
+
+	generateTopics(yaks_dot_txt)
+
+
